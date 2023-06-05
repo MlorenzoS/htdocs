f10413fc16d9e31a222bb343f56c8f0717122c69
Mauricio Lorenzo <118038386+MlorenzoS@users.noreply.github.com>
2023-06-05 18:19:52 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$db_name = "mercadinho";
+$servidor = "us-cdbr-east-06.cleardb.net";
+$usuario = "b93973f17777ee";
+$senha = "78b5941e";
+$db_name = "heroku_1003938d3b1de4e";
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $db_name);
 
